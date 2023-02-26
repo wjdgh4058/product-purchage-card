54e5ac00e94e2c130094f1c42e18bec5145966a9
leejungho <ljhmoa@gmail.com>
2023-02-26 10:59:51 +0900

    [Feature] counter 버튼 완성

--- src/component/PurchageView/PurchageView.jsx
+++ src/component/PurchageView/PurchageView.jsx
@@ -1,13 +1,19 @@
 // PurchageView 기본 틀
 import "./PurchageView.css";
 import { Counter, DeliveryInfo, ProductInfo } from "./component/index";
+import { useState } from "react";
 
 export function PurchageView(props) {
+  const [count, setCount] = useState(1);
   return (
     <div className="pulchageView">
       <ProductInfo product={props.productInfo}></ProductInfo>
       <DeliveryInfo product={props.productInfo}></DeliveryInfo>
-      <Counter product={props.productInfo}></Counter>
+      <Counter
+        product={props.productInfo}
+        count={count}
+        setCount={setCount}
+      ></Counter>
     </div>
   );
 }
--- src/component/PurchageView/component/Counter.jsx
+++ src/component/PurchageView/component/Counter.jsx
@@ -1,3 +1,35 @@
-export function Counter() {
-  return <div>카운터 만들깅</div>;
+export function Counter(props) {
+  const minusBtn = () => {
+    if (props.count > 0) {
+      props.setCount(props.count - 1);
+    }
+  };
+
+  const plusBtn = () => {
+    if (props.count < props.product.maxAmount) {
+      props.setCount(props.count + 1);
+    }
+  };
+
+  return (
+    <div className="counter">
+      <button
+        className="counter__minusBtn"
+        aria-label="상품 수량 감소"
+        onClick={minusBtn}
+      >
+        -
+      </button>
+      <output className="counter__output" aria-label="선택한 상품 수량">
+        {props.count}
+      </output>
+      <button
+        className="counter__plusBtn"
+        aria-label="상품 수량 증가"
+        onClick={plusBtn}
+      >
+        +
+      </button>
+    </div>
+  );
 }
